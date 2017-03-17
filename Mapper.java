1017b7bf03a6ca38e57bd9294fbe96b05b52ba77
xiaogang00 <872310734@qq.com>
2017-03-17 23:48:54 +0800

    the notes when learn hadoop

--- Mapper.java
+++ Mapper.java
@@ -4,6 +4,8 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
+import jdk.nashorn.internal.runtime.arrays.IteratorAction;
+
 public class MaxTemperatureMapper
   extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWtitable>{
     private static final int MISSING = 9999;
@@ -25,4 +27,54 @@ public class MaxTemperatureMapper
         }
       }
   }
-  
+
+
+//Reducer 类
+public class MaxTemperatureReducer
+   extends Reducer<Text, IntWritable, Text, IntWritable>{
+     @Override
+     public void reduce(Text key, Iterable<IntWritable> values, Context context)
+        throws IOException, InterruptedException{
+          int maxValue = Integer.MIN_VALUE;
+          for (IntWritable value :values){
+            maxValue = Math.max(maxValue, value.get());
+          }
+          context.write(key, new IntWritable(maxValue));
+     }
+   }
+//应用这个程序在气象数据集中找出最高气温
+import java.io.IOException;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.input.FileOutputFormat;
+import org.apache.hadoop.mapredduce.input.FileOutputFormat;
+
+public class MaxTemperature{
+  public static void main(String[] args) throws Exception{
+    if(args.length != 2){
+      System.err.println("Usage: MaxTemperature <input path> <output path>");
+      System.exit(-1);
+    }
+    Job job = new Job();
+    job.setJarByClass(MaxTemperature.class);
+    job.setJobName("Max Temperature");
+    
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    job.setMapperClass(MaxTemperatureMapper.class);
+    job.setReducerClass(MaxTemperatureReducer.class);
+
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
+}
+
+
+
+
+
+
+
--- 关于MapReduce.md
+++ 关于MapReduce.md
@@ -40,4 +40,19 @@ public class MaxTemperatureMapper
 
 Reduce类也是按照相似的方法进行定义，而Job对象指定作业执行规范。我们可以用它来控制整个作业的运行。我们再Hadoop集群上运行这个作业的时候，我们要把代码打包成一个JAR文件。
 
-构建Job对象之后，需要指定输入和输出数据的路径。
+构建Job对象之后，需要指定输入和输出数据的路径。可以调用FileInputFormat类的静态方法addInputPath来定义输入数据的路径。这个路径可以是单个的文件。也可以用多次调用addInputPath()来实现多路径的输入。
+
+setOutputKeyClass和setOutputValueClass控制map和reduce函数的输出类型，一般情况下都应该是相同的。
+
+Job中的waitForCompletion( ) 方法提交作业并且等待执行完成。该方法中的布尔参数是一个详细标识，所以作业会把进度写到控制台。这个方法返回一个布尔值，表示执行的成ture败false
+
+
+
+新旧的API之间有明显的区别：
+
+* 新的API倾向于使用虚类，而不是接口，因为更加容易拓展
+* 新的API放在org.apache.hadoop.mapreduce包中，之前的版本API依旧放在org.apache.hadoop.mapred中
+* 新的API充分使用上下文对象，使得用户代码能够与MapReduce系统之间进行通信
+* 键值对记录在这两类API中都被推给mapper和reducer。但是除此之外，新的API通过重写run( )方法允许mapper和reducer控制执行流程。
+* 新的API中作业控制由Job类实现，而非旧API中的JobClient类
+* 新增的API实现了配置的统一
